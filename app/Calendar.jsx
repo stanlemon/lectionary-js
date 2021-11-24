60dd22f96c86320d1e164d72ed9d3eb9ff3f33bc
Stan Lemon <stanlemon@users.noreply.github.com>
2021-11-24 17:43:10 -0500

    Removed unused types

--- app/Calendar.jsx
+++ app/Calendar.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import { isRequiredNumeric } from "./propTypes/isNumeric";
 import { DateTime } from "luxon";
 import { Link } from "wouter";
