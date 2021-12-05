d2307a72b69413e0817dae63b1e97bacb036dbd7
Stan Lemon <stanlemon@users.noreply.github.com>
2021-12-05 11:47:49 -0500

    Switch to standardjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-import webpack from "webpack";
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-const env = process.env.NODE_ENV;
-const publicPath = process.env.PUBLIC_PATH || "/";
-const isProduction = env === "production";
+const env = process.env.NODE_ENV
+const publicPath = process.env.PUBLIC_PATH || '/'
+const isProduction = env === 'production'
 
 export default {
-  mode: env || "development",
-  devtool: isProduction ? "source-map" : "eval-source-map",
-  entry: ["./app/App.jsx"],
+  mode: env || 'development',
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
+  entry: ['./app/App.jsx'],
   output: {
-    path: path.resolve("./", "dist"),
-    publicPath,
+    path: path.resolve('./', 'dist'),
+    publicPath
   },
   devServer: {
     hot: true,
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   module: {
     rules: [
@@ -24,37 +24,37 @@ export default {
         test: /\.([j|t]s)x?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.(jpg|png)$/,
         use: {
-          loader: "url-loader",
-        },
-      },
-    ],
+          loader: 'url-loader'
+        }
+      }
+    ]
   },
   resolve: {
     // Enable webpack to find files without these extensions
-    extensions: [".jsx", ".js"],
+    extensions: ['.jsx', '.js']
   },
   plugins: [
     new webpack.DefinePlugin({
-      PUBLIC_PATH: JSON.stringify(publicPath),
+      PUBLIC_PATH: JSON.stringify(publicPath)
     }),
     new HtmlWebpackPlugin({
       minify: false,
       publicPath,
-      filename: "index.html",
-      template: path.resolve("./", "app", "index.html"),
+      filename: 'index.html',
+      template: path.resolve('./', 'app', 'index.html')
     }),
     new HtmlWebpackPlugin({
       minify: false,
       inject: false,
       publicPath,
-      filename: "404.html",
-      template: path.resolve("./", "app", "404.html"),
-    }),
-  ],
-};
+      filename: '404.html',
+      template: path.resolve('./', 'app', '404.html')
+    })
+  ]
+}
